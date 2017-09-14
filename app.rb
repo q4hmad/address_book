e49a82763e50f80559da43826188fa45b06a4cb3
Qudsia Ahmad and Rebecca McIntosh <rebecca@>
2017-09-14 14:14:12 -0700

    commit working back end that pass rspecs

---
+++ app.rb
@@ -0,0 +1,22 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/address_book')
+require('pry')
+
+get('/') do
+  @input = Contact.all()
+  erb(:input)
+end
+
+post('/') do
+  first_name = params['first_name']
+  last_name = params['last_name']
+  job_title = params['job_title']
+  company = params['company']
+  contact_type = params['contact_type']
+  contact = Contact.new(:first_name=> first_name, :last_name=> last_name, :job_title=> job_title, :company=> company, :contact_type=> contact_type)
+  contact.save()
+  @input = Contact.all()
+  erb(:input)
+end
---
+++ spec/address_book_spec.rb
@@ -0,0 +1,34 @@
+require('rspec')
+require('address_book')
+
+describe('Contact') do
+  describe("Contact") do
+  before() do
+    Contact.clear()
+  end
+  describe('.all') do
+    it('is empty at first') do
+      expect(Contact.all()).to(eq([]))
+    end
+  end
+
+describe('#save') do
+  it('saves an item to the list of items') do
+    contact = Contact.new({:first_name=> 'Sebastian', :last_name=> 'Biggs-McIntosh', :job_title=> 'professional-thrower', :company=> 'GrandmasPlace', :contact_type=> 'professional-pooper'})
+    contact.save()
+    expect(contact.first_name()).to(eq('Sebastian'))
+   end
+ end
+
+ describe('.clear') do
+   it('clears all contacts from the list') do
+    contact = Contact.new({:first_name=> 'Sebastian', :last_name=> 'Biggs-McIntosh', :job_title=> 'professional-thrower', :company=> 'GrandmasPlace', :contact_type=> 'professional-pooper'})
+     contact.save()
+     Contact.clear()
+     expect(Contact.all()).to(eq([]))
+   end
+ end
+
+
+
+end
